f8e3a3fe4d51e3336923e0b0178536a05b3930c1
uniqsaqya <uniqsaqya@gmail.com>
2016-03-11 22:55:50 +0545

    directory commit first

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,7 +15,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $this->app['view']->setFinder($this->app['theme.finder']);
     }
 
     /**
